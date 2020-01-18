511e2d4858d697867e558e05f0caa49cac363496
YourFigo <501385003@qq.com>
2020-01-18 23:14:19 +0800

    实现调用方的远程调用，通过提供方的rest接口来访问数据

---
+++ figo_service_provider/src/main/java/cn/figo/service/FigoServiceProviderApplication.java
@@ -0,0 +1,15 @@
+package cn.figo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("cn.figo.service.mapper") //mapper接口的包扫描
+public class FigoServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigoServiceProviderApplication.class, args);
+	}
+
+}
