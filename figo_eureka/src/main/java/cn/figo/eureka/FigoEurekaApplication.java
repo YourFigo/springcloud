008d775cca53701e153a2e889aa3cb112ea35ca9
YourFigo <501385003@qq.com>
2020-01-19 22:46:34 +0800

    SpringCloud Eureka

---
+++ figo_eureka/src/main/java/cn/figo/eureka/FigoEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.figo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigoEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigoEurekaApplication.class, args);
+	}
+
+}
