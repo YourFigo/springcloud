0b0e544d61b7eae48a6d03b5dc58e7af876d239e
YourFigo <501385003@qq.com>
2020-01-19 23:16:45 +0800

    SpringCloud Eureka  在customer中配置eureka

--- figo_service_customer/src/main/java/cn/figo/service/controller/UserController.java
+++ figo_service_customer/src/main/java/cn/figo/service/controller/UserController.java
@@ -1,6 +1,7 @@
 package cn.figo.service.controller;
 
 import cn.figo.service.pojo.User;
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,8 @@ public class UserController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+
 
     @GetMapping
     @ResponseBody
