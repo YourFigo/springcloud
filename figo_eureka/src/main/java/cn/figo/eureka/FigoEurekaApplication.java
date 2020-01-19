e13d3bb93e5de3373e42603ca45d95e67ace5882
YourFigo <501385003@qq.com>
2020-01-19 22:54:13 +0800

    SpringCloud Eureka  开启服务

--- figo_eureka/src/main/java/cn/figo/eureka/FigoEurekaApplication.java
+++ figo_eureka/src/main/java/cn/figo/eureka/FigoEurekaApplication.java
@@ -2,8 +2,10 @@ package cn.figo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  //启用Eureka服务端
 public class FigoEurekaApplication {
 
 	public static void main(String[] args) {
