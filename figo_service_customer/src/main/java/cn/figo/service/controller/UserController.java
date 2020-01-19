9693dd5d266e2f15dd41cfb09a7ed9bd7ead7614
YourFigo <501385003@qq.com>
2020-01-19 23:24:08 +0800

    SpringCloud Eureka  在customer获取provider中的服务列表

--- figo_service_customer/src/main/java/cn/figo/service/controller/UserController.java
+++ figo_service_customer/src/main/java/cn/figo/service/controller/UserController.java
@@ -1,14 +1,17 @@
 package cn.figo.service.controller;
 
 import cn.figo.service.pojo.User;
-import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @Author Figo
@@ -21,11 +24,16 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
-
+    // 拉去服务提供方的所有服务列表
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping
     @ResponseBody
     private User queryUserById(@RequestParam("id") Long id){
-        return this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
+        List<ServiceInstance> instances = discoveryClient.getInstances("service_provider");
+        ServiceInstance serviceInstance = instances.get(0);
+        return this.restTemplate.getForObject("http://" + serviceInstance.getHost() +":"+serviceInstance.getPort()+"/user/" + id, User.class);
+//        return this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
     }
 }
