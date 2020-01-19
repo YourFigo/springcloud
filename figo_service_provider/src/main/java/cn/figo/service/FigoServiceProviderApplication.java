2b5c644369b6c49469aec1cd764a662403089702
YourFigo <501385003@qq.com>
2020-01-19 23:03:23 +0800

    SpringCloud Eureka  在provider中配置eureka

--- figo_service_provider/src/main/java/cn/figo/service/FigoServiceProviderApplication.java
+++ figo_service_provider/src/main/java/cn/figo/service/FigoServiceProviderApplication.java
@@ -2,10 +2,12 @@ package cn.figo.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("cn.figo.service.mapper") //mapper接口的包扫描
+@EnableDiscoveryClient  //启用eureka客户端，也可以用 @EnableEurekaClient
 public class FigoServiceProviderApplication {
 
 	public static void main(String[] args) {
