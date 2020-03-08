1e429cc2dd6221fc629ce55c3d5ef660eddfa329
YourFigo <501385003@qq.com>
2020-03-08 20:34:50 +0800

    SpringCloud zuul initial

---
+++ figo_zuul/src/main/java/cn/figo/figo_zuul/FigoZuulApplication.java
@@ -0,0 +1,13 @@
+package cn.figo.figo_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigoZuulApplication.class, args);
+	}
+
+}
